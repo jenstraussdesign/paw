3984882a0615c84c8949c99d744aada2dd113e8e
Jen Strauss <jenstraussdesign@gmail.com>
2016-03-24 20:18:10 -0400

    jen's update

--- main.js
+++ main.js
@@ -4,12 +4,14 @@ var total = 4;
 setInterval(function slideA() {
     var Image = document.getElementById('img');
     imagecount = imagecount + 1;
-      if (imagecount > total) {
-        imagecount = 1;
-      }
-      if (imagecount < 1) {
-        imagecount = total;
-      }
-    Image.src ="images/img"+ imagecount + ".jpg";
+    if (imagecount > total) {
+      imagecount = 1;
+    }
 
-  },7000);
+    if (imagecount < 1) {
+      imagecount = total;
+    }
+
+    Image.src = 'images/img' + imagecount + '.jpg';
+
+  }, 7000);
